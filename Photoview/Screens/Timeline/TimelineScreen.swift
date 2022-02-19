7ee85eee7ffba600ee37b5f17e77847c9817f04c
viktorstrate <viktorstrate@gmail.com>
2022-02-19 23:09:49 +0100

    Refactor timeline state

--- Photoview/Screens/Timeline/TimelineScreen.swift
+++ Photoview/Screens/Timeline/TimelineScreen.swift
@@ -9,73 +9,29 @@ import SwiftUI
 
 struct TimelineScreen: View {
     
-    @State var timelineData: [TimelineQuery.Data.MyTimeline]? = nil
+    @StateObject var timelineState: TimelineState = TimelineState()
+    
     @EnvironmentObject var showWelcome: ShowWelcomeScreen
     
-    @State var offset = 0
-    let limit = 200
-    @State var moreToLoad = true
-    @State var loading = false
-    
-    func fetchTimeline() {
-        timelineData = []
-        offset = 0
-        moreToLoad = true
-        loadMore()
-    }
-    
-    func loadMore() {
-        if !moreToLoad || loading {
-            return
-        }
-        
-        loading = true
-        Network.shared.apollo?.fetch(query: TimelineQuery(limit: limit, offset: offset)) { result in
-            switch (result) {
-            case let .success(data):
-                DispatchQueue.main.async {
-                    if (data.data?.myTimeline ?? []).isEmpty {
-                        moreToLoad = false
-                    }
-                    
-                    if var timelineData = self.timelineData {
-                        timelineData.append(contentsOf: data.data?.myTimeline ?? [])
-                        self.timelineData = timelineData
-                        print("load more appended, new size: \(timelineData.count)")
-                    } else {
-                        self.timelineData = data.data?.myTimeline
-                    }
-                    
-                    offset += limit
-                }
-            case let .failure(error):
-                Network.shared.handleGraphqlError(error: NetworkError(message: "Failed to fetch timeline", error: error), showWelcomeScreen: showWelcome)
-            }
-            DispatchQueue.main.async {
-                loading = false
-            }
-        }
-    }
-    
-    @ViewBuilder
-    var content: some View {
-        if let timelineData = timelineData {
-            TimelineView(timelineData: timelineData, loadMore: loadMore)
-        } else {
-            ProgressView("Loading timeline")
+    func fetchTimeline() async {
+        do {
+            try await timelineState.fetchTimeline()
+        } catch {
+            Network.shared.handleGraphqlError(error: NetworkError(message: "Failed to fetch timeline", error: error),
+                                              showWelcomeScreen: showWelcome)
         }
     }
     
     var body: some View {
-        content
-        .onAppear {
-            if (timelineData == nil) {
-                fetchTimeline()
+        TimelineView(timelineState: timelineState)
+        .task {
+            if (timelineState.timelineData == nil) {
+               await fetchTimeline()
             }
         }
         .onChange(of: showWelcome.isPresented) { _ in
             if !showWelcome.isPresented {
-                fetchTimeline()
+                Task { await fetchTimeline() }
             }
         }
     }
--- Photoview/Screens/Timeline/TimelineView.swift
+++ Photoview/Screens/Timeline/TimelineView.swift
@@ -6,106 +6,12 @@
 //
 
 import SwiftUI
-import Apollo
-
-class TimelineState: ObservableObject {
-    @Published var mediaEnvs: [TimelineAlbumGroup: MediaEnvironment] = [:]
-    @Published var timelineAlbums: [TimelineAlbumGroup] = []
-    
-    func refreshTimeline(timelineData: [TimelineQuery.Data.MyTimeline]) {
-        print("Refreshing timeline: \(timelineData.count)")
-        let newTimelineAlbums = self.calculateTimelineAlbums(timelineData: timelineData)
-        let newMediaEnvs = self.buildMediaEnvs(timelineAlbums: newTimelineAlbums)
-        
-        self.timelineAlbums = newTimelineAlbums
-        self.mediaEnvs = newMediaEnvs
-        print("Timeline refreshed")
-    }
-    
-    private func calculateTimelineAlbums(timelineData: [TimelineQuery.Data.MyTimeline]) -> [TimelineAlbumGroup] {
-        var albums: [TimelineAlbumGroup] = []
-        
-        guard let firstMedia = timelineData.first else {
-            return []
-        }
-        
-        var nextAlbum: TimelineAlbumGroup = TimelineAlbumGroup(albumID: firstMedia.album.id, title: firstMedia.album.title, day: Calendar.current.startOfDay(for: firstMedia.date.date), media: [firstMedia])
-        
-        for media in timelineData.dropFirst() {
-            let mediaDay = Calendar.current.startOfDay(for: media.date.date)
-            
-            if mediaDay == nextAlbum.day && media.album.id == nextAlbum.albumID {
-                nextAlbum.media.append(media)
-            } else {
-                albums.append(nextAlbum)
-                nextAlbum = TimelineAlbumGroup(albumID: media.album.id, title: media.album.title, day: Calendar.current.startOfDay(for: media.date.date), media: [media])
-            }
-        }
-        
-        albums.append(nextAlbum)
-        
-        return albums
-    }
-    
-    private func buildMediaEnvs(timelineAlbums: [TimelineAlbumGroup]) -> [TimelineAlbumGroup: MediaEnvironment] {
-        var result: [TimelineAlbumGroup: MediaEnvironment] = [:]
-        
-        for albumGroup in timelineAlbums {
-            //      let media = try! albumGroup.media.map(MediaEnvironment.Media.from)
-            let media = albumGroup.media.map { $0.fragments.mediaItem }
-            result[albumGroup] = MediaEnvironment(media: media, activeMediaIndex: 0)
-        }
-        print("Finished building media envs: \(result.count)")
-        return result
-    }
-}
-
-struct TimelineAlbumGroup: Identifiable, Hashable {
-    var id: Self {
-        self
-    }
-    
-    let albumID: GraphQLID
-    let title: String
-    let day: Date
-    var media: [TimelineQuery.Data.MyTimeline]
-    
-    static func == (lhs: TimelineAlbumGroup, rhs: TimelineAlbumGroup) -> Bool {
-        return lhs.albumID == rhs.albumID && lhs.day == rhs.day
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(albumID)
-        hasher.combine(day)
-    }
-}
 
 struct TimelineView: View {
-    
-    let timelineData: [TimelineQuery.Data.MyTimeline]
-    let loadMore: () -> Void
-    @StateObject var timelineState: TimelineState = TimelineState()
+    @ObservedObject var timelineState: TimelineState
+    @EnvironmentObject var showWelcome: ShowWelcomeScreen
     
     @State var activeMediaEnv: MediaEnvironment?
-    @State var loadedFirst = false
-    
-    func loadMoreIfNeeded(albumGroup: TimelineAlbumGroup, mediaIndex: Int) {
-        var mediaCount = 0
-        
-        for x in self.timelineState.timelineAlbums {
-            if x == albumGroup { break }
-            mediaCount += x.media.count
-        }
-        
-        mediaCount += mediaIndex
-        
-        let totalCount = timelineData.count
-        
-        print("Load more: \(totalCount) - \(mediaCount)")
-        if totalCount - mediaCount < 20 {
-            loadMore()
-        }
-    }
     
     let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -134,7 +40,14 @@ struct TimelineView: View {
         ) {
             if let mediaEnv = timelineState.mediaEnvs[albumGroup] {
                 MediaGrid(onMediaAppear: { media in
-                    loadMoreIfNeeded(albumGroup: albumGroup, mediaIndex: mediaEnv.mediaIndex(media))
+                    Task {
+                        do {
+                            try await timelineState.loadMoreIfNeeded(albumGroup: albumGroup, mediaIndex: mediaEnv.mediaIndex(media))
+                        } catch {
+                            Network.shared.handleGraphqlError(error: NetworkError(message: "Failed to fetch timeline", error: error),
+                                                              showWelcomeScreen: showWelcome)
+                        }
+                    }
                 }, onMediaSelected: { _ in
                     self.activeMediaEnv = mediaEnv
                 })
@@ -154,17 +67,11 @@ struct TimelineView: View {
             }
             .navigationTitle("Timeline")
         }
-        .onAppear {
-            self.timelineState.refreshTimeline(timelineData: self.timelineData)
-        }
-        .onChange(of: timelineData) { newData in
-            self.timelineState.refreshTimeline(timelineData: newData)
-        }
     }
 }
 
 struct TimelineView_Previews: PreviewProvider {
     static var previews: some View {
-        TimelineView(timelineData: [], loadMore: {})
+        TimelineView(timelineState: TimelineState())
     }
 }
