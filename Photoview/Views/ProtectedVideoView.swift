31a81285968703ee87d459e46bcf137680e9329f
Dhrumil Shah <ddshd@users.noreply.github.com>
2023-03-01 12:08:47 -0500

    WIP: Updated for the new fullscreen player

--- Photoview/Views/ProtectedVideoView.swift
+++ Photoview/Views/ProtectedVideoView.swift
@@ -12,6 +12,9 @@ import AVKit
 struct ProtectedVideoView: View {
     let url: String?
     
+    @Binding var isLoading: Bool
+    @State var willBeginFullScreenPresentation: Bool = false
+
     var videoAsset: AVURLAsset? {
         guard let url = self.url else { return nil }
         
@@ -29,7 +32,7 @@ struct ProtectedVideoView: View {
         } else {
             player = nil
         }
-        
-        return VideoPlayer(player: player)
+        return FullscreenAVPlayerView(player: player!)
+        .shadow(radius: 0)
     }
 }
